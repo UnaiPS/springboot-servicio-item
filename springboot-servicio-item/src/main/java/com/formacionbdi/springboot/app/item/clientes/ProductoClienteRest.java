a06af83eb9842b4433a0e7a2195f7740b202f61f
Unai Perez <uperez@certiorem.com>
2020-03-04 14:34:51 +0100

    creado un CRUD para consumir Producto desde el microservicio de Producto

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
@@ -3,10 +3,16 @@ package com.formacionbdi.springboot.app.item.clientes;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.formacionbdi.springboot.app.item.models.Producto;
+
+import feign.Body;
 
 @FeignClient(name = "servicio-productos")
 public interface ProductoClienteRest {
@@ -16,5 +22,14 @@ public interface ProductoClienteRest {
 	
 	@GetMapping("/ver/{id}")
 	public Producto detalle (@PathVariable Long id);
+	
+	@PostMapping("/crear")
+	public Producto crear(@RequestBody Producto producto);
+	
+	@PutMapping("/editar/{id}")
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id);
+	
+	@DeleteMapping("/eliminar/{id}")
+	public void eliminar(@PathVariable Long id);
 	
 }
